4b64e45d6a600d43e186239b46acf82ec8e0148a
airzhanglin <1015448230@qq.com>
2019-04-29 09:34:12 +0800

    update

--- src/wechatPlayer/index.js
+++ src/wechatPlayer/index.js
@@ -7,9 +7,13 @@ import wechatPlayer from './wechatPlayer.vue'
 wechatPlayer.install = function(Vue){
     Vue.component(wechatPlayer.name,wechatPlayer);
 }
-/* 支持使用标签的方式引入 */
-if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
-  }
+// 标签的方式引入，留到后面再另开新篇讨论
+  //const install = function(Vue, opts = {}) {
+　//　　Vue.component(wechatPlayer.name, wechatPlayer);
+　//}
+/* 支持使用标签的方式引入 Vue是全局变量时，自动install */
+//if (typeof window !== 'undefined' && window.Vue) {
+//  install(window.Vue);
+//}
 
 export default wechatPlayer
